f531f64b87a82a8818b357c9e6a99cd2bae588fd
lilinjian <Li@123456>
2024-07-24 16:12:55 +0800

    1.01

--- src/main/java/com/webdemo/springbootwebdemo/service/impl/UserServiceImpl.java
+++ src/main/java/com/webdemo/springbootwebdemo/service/impl/UserServiceImpl.java
@@ -32,7 +32,7 @@ public class UserServiceImpl implements UserService {
     }
 
     @Override
-    public void update(                                                                                                  User user) {
+    public void update(User user) {
 
         user.setUpdateTime(LocalDateTime.now());
         userMapper.update(user);
